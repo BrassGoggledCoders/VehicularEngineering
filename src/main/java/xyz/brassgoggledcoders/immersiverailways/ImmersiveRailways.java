2f595abab192e8d92d416bd307eb5d8b0a6523dd
Skylar Sommers <ssommers@criticalmix.com>
2016-07-08 23:21:23 -0400

    Added first file

---
+++ src/main/java/xyz/brassgoggledcoders/immersiverailways/ImmersiveRailways.java
@@ -0,0 +1,12 @@
+package xyz.brassgoggledcoders.immersiverailways;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = ImmersiveRailways.MODID, name = ImmersiveRailways.NAME, version = ImmersiveRailways.VERSION)
+public class ImmersiveRailways {
+	public static final String MODID = "immersiverailways";
+	public static final String NAME = "Immersive Railways";
+	public static final String VERSION = "@VERSION@";
+
+
+}
